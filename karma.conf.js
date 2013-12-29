ad64459019301318ada85457b0ab11bba254ae9b
Duncan Beaton <d@duncky.com>
2013-12-29 13:07:16 +0000

    add karma growl

--- karma.conf.js
+++ karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
+    reporters: ['progress','growl'],
 
     // Start these browsers, currently available:
     // - Chrome
